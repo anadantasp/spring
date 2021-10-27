b1a4d6ab6e48fb56842d38084cf71d3ba203c1fc
anadantasp <caroldantasp@gmail.com>
2021-10-27 18:24:17 -0300

    Adicionando exercícios de Spring

---
+++ aula_01/habilidades_mentalidades/src/test/java/br/org/generation/habilidades_mentalidades/HabilidadesMentalidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.generation.habilidades_mentalidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabilidadesMentalidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ aula_01/objetivos_aprendizagem/src/main/java/br/org/generation/objetivos_aprendizagem/ObjetivosAprendizagemApplication.java
@@ -0,0 +1,13 @@
+package br.org.generation.objetivos_aprendizagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjetivosAprendizagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjetivosAprendizagemApplication.class, args);
+	}
+
+}
